ef1b1fc029d7f430a449704db5f293885247bbc9
unknown <liannedelasalle@gmail.com>
2020-04-05 19:57:19 -0500

    Updates

---
+++ precache-manifest.d3f1d448ef221ebcca2e938e10266277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fdf630cf5a0b830661fe2a43f73ad82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f4300ef8637ed622992",
+    "url": "/static/css/main.32de09b1.chunk.css"
+  },
+  {
+    "revision": "214d658661815f498393",
+    "url": "/static/js/2.d0a690dd.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/static/js/2.d0a690dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f4300ef8637ed622992",
+    "url": "/static/js/main.54963ca1.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  }
+]);
